35d30bd1d2a89fda80ff15fb6e89677e25474f6a
Emilie <92803520+SakoSuta@users.noreply.github.com>
2023-06-06 02:03:51 +0200

    new page and route: - login (finish font) - register
    new logo, icon and variable

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
+import RegisterPage from '../views/Auth/Register.vue'
+import LoginPage from '../views/Auth/Login.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,6 +13,16 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginPage
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: RegisterPage
   }
 ]
 
---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,112 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <div class="navlog"><img src="Very_long_item.png" alt="Long Logo"></div>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <div class="navlog"><img src="Very_long_item.png" alt="Long Logo"></div>
+        </ion-toolbar>
+      </ion-header>
+
+      <div class="Go_Back" onclick="history.back()">
+        <img src="Go_Back.png" alt="Return button">
+      </div>
+      <div class="FormuLogin">
+        <form action="">
+          <h1 class="Gugi">Login</h1>
+          <input type="email" placeholder="Email" class="Gugi">
+          <input type="password" placeholder="Password" class="Gugi">
+          <button class="Gugi">Connect</button>
+        </form>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+</script>
+
+<style>
+.Gugi{
+  font-family: var(--font-gugi);
+}
+
+.navlog{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 74px;
+  background-color: #24223E;
+}
+
+.FormuLogin{
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+  height: 100%;
+  background-color: #242131;
+  padding: 10px 0px;
+  width: 100%;
+  height: 100%;
+}
+
+h1{
+  font-size: 28px !important;
+}
+
+form{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 70%;
+  height: 40%;
+}
+
+input{
+  width: 100%;
+  height: 40px;
+  margin: 18px;
+  padding-left: 20px;
+  border-radius: 9px;
+  border: none;
+  background-color: #2F2B45;
+  color: #9987FF !important;
+  font-size: 12px;
+}
+
+input::placeholder{
+  color: #6459A2 !important;
+  font-size: 12px;
+}
+
+input:focus{
+  outline: none;
+}
+
+button{
+  width: 80%;
+  height: 34px;
+  margin: 18px;
+  border-radius: 9px !important;
+  border: none !important;
+  background-color: #9987FF;
+  color: #fff !important;
+  font-size: 12px;
+}
+
+.Go_Back{
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 90px;
+  background-color: #242131;
+  padding-left: 25px;
+}
+</style>
