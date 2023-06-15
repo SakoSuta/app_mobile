dc4a1e371c9d0de45e378531f649773a3513d31e
Emilie <92803520+SakoSuta@users.noreply.github.com>
2023-06-15 23:32:09 +0200

    New formulaire for edit profile

--- src/views/User.vue
+++ src/views/User.vue
@@ -7,20 +7,37 @@
       </div>
       <div class="AllContUser">
         <h1 class="titleUser">Hi, User</h1>
-        <div class="CardUser">
-          <img src="https://loremflickr.com/320/240" alt="Profile Photo" class="UserPic"/>
-          <div class="UserInfo">
-            <div class="UserName">
-              <h2>Pseudo</h2>
-              <span>User</span>
+        <div style="width: 100%;">
+          <div class="CardUser" v-if="EditON">
+            <img src="https://loremflickr.com/320/240" alt="Profile Photo" class="UserPic"/>
+            <div class="UserInfo">
+              <div class="UserName">
+                <h2>Pseudo</h2>
+                <span>User</span>
+              </div>
+              <div class="OtherInfo">
+                <h2>EmilieMontpre@outlook.com</h2>
+                <h2>Subscriptions : none</h2>
+              </div>
+              <div class="ContEdit">
+                <button @click="EditON = !EditON" class="EditButton">Edit</button>
+              </div>
             </div>
-            <div class="OtherInfo">
-              <h2>EmilieMontpre@outlook.com</h2>
-              <h2>Subscriptions : none</h2>
+          </div>
+          <div class="CardUser" v-else>
+            <div class="ImageUser">
+              <img src="https://loremflickr.com/320/240" alt="Profile Photo" class="UserPic"/>
+              <router-link to="/Login" class="EditPict"><img src="Icone/Edit.svg" alt="Edit icone"></router-link>
             </div>
-            <div class="ContEdit">
-              <router-link to="/Users/cvgbhjzdq5" class="EditButton">Edit</router-link>
-            </div>
+            <form action="" class="EditForm">
+              <input type="text" placeholder="Name">
+              <input type="text" placeholder="Pseudo">
+              <input type="text" placeholder="Email">
+              <div class="ContEditF">
+                <button @click="EditON = !EditON" class="CancelButton">Cancel</button>
+                <button type="submit" class="EditButton">Edit</button>
+              </div>
+            </form>
           </div>
         </div>
         <router-link to="/Login" class="LogoutButton">Logout</router-link>
@@ -31,6 +48,10 @@
 
 <script setup lang="ts">
 import { IonContent, IonPage } from "@ionic/vue";
+
+import { ref } from 'vue'
+
+const EditON = ref(true)
 </script>
 
 <style>
@@ -48,6 +69,7 @@ import { IonContent, IonPage } from "@ionic/vue";
   color: #ffffff;
   padding: 0px 24px;
   margin: 0px;
+  margin-bottom: 30px;
   width: 100%;
 }
 .CardUser {
@@ -113,6 +135,64 @@ import { IonContent, IonPage } from "@ionic/vue";
   padding: 9px 44px;
   background-color: #24223e;
   color: #e42f2f;
+  border-radius: 9px;
+}
+
+.ImageUser{
+  display: flex;
+}
+.EditPict{
+  position: relative;
+  bottom: 30px;
+  right: 62px;
+  width: 62px;
+  height: 62px;
+  border-radius: 50%;
+  background-color: rgba(153, 135, 255, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.EditForm{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+.EditForm input{
+  background-color: #2F2C51;
+  width: 100%;
+  height: 40px;
+  border-radius: 9px;
+  border: none;
+  padding: 0px 10px;
+  margin: 10px 0px;
+  color: #ffffff;
+  font-family: var(--font-gugi);
+  font-size: 12px;
+}
+.EditForm input::placeholder {
+  color: #6459a2 !important;
+  font-size: 12px;
+}
+.EditForm input:focus {
+  outline: none;
+}
+.ContEditF{
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20px; 
+  width: 100%;
+}
+.CancelButton{
+  width: min-content;
+  padding: 6px 16px;
+  font-family: var(--font-gugi);
+  font-size: 12px;
+  text-align: center;
+  text-decoration: none;
+  background-color: #242252;
+  color: #5E54A8;
   border-radius: 9px;
 }
 </style>
